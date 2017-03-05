137a75618d8da2e4467e24c7f78845a3aeb369ea
light-of-king <light_of_king@sina.com>
2017-03-05 14:33:04 +0800

    supported all version
    supported all version

--- ios/RNZipArchive.h
+++ ios/RNZipArchive.h
@@ -5,8 +5,11 @@
 //  Created by Perry Poon on 8/26/15.
 //  Copyright (c) 2015 Perry Poon. All rights reserved.
 //
-
+#if __has_include(<React/RCTAssert.h>)
 #import <React/RCTBridgeModule.h>
+#else
+#import "RCTBridgeModule.h"
+#endif
 #import "SSZipArchive/SSZipArchive.h"
 
 @interface RNZipArchive : NSObject<RCTBridgeModule, SSZipArchiveDelegate>
